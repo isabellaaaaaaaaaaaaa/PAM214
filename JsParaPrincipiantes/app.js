158ea5a0e86304d8aadfff1cded05d18aae6cb2f
isabellaaaaaaaaaaaaa <isabella.castal12@gmail.com>
2025-09-10 18:37:29 -0600

    Avance1VidJs

---
+++ JsParaPrincipiantes/app.js
@@ -0,0 +1,23 @@
+// abrir en google "about:blank" - más herramientas - herramientas del desarrollador. Se utiliza la consola. Console. la acción que tiene que hacer. "log" recibe el texto que queremos que muestre
+// Java altera las acciones de una página. document.write para rscribir el el documenmt.write
+// util para pss web no pags
+//console.error : crear un erro
+
+//Tipos de datos. solo se están malipulad¿ndo datos constantemente
+
+//"Hello world " // string = cadena de caracteres
+//'Hello world' // string de otra manera
+
+//1000 // Number hacer operaciones dentro de las paginas
+//10.001 // Number
+//-2.3  //number
+
+//true
+//false// boolean = como estados de verdadero y falso
+
+//datos lógicos tmb hay items ['joe ' ,'ryan', 'martha'] //array = arreglos, cada usuario es un string, tmb se puede con números, o con datos booleano[ 1, 2, 3[true, false, true, false]
+
+//'ryan' // object = objetos, representación de un objeto en la vida real 70.4 14
+//true // diferentes datos que pertenecnen a la misma entidad 
+
+console.log ([1, 2, 3]);
