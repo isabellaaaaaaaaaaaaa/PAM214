3d3a6bd5c3716af477e610e363ee8b0aad2c16dc
isabellaaaaaaaaaaaaa <isabella.castal12@gmail.com>
2025-09-17 20:57:30 -0600

    EjercicioBPractica1

--- Practica1/variables&funciones.js
+++ Practica1/variables&funciones.js
@@ -1,11 +1,5 @@
 // PRÁCTICA 1
 
-//EJERCICIO A (cambiar los var)
-let nombre = "Armando";
-let edad = 25;
-
-nombre = "Ana Maria";
-
-let saludo = "Hola, " + nombre + ". Tienes " + edad + "años"; 
-
-console.log (saludo);
+//EJERCICIO B (tradicional a flehca)
+const multiplicación = (numero) => numero*numero;
+console.log(multiplicación(12735))
