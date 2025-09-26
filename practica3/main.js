6c54e4162f052fe0bef91fe11b03cc7c800b4482
isabellaaaaaaaaaaaaa <isabella.castal12@gmail.com>
2025-09-25 22:48:21 -0600

    ejercicio B prac 3

--- practica3/main.js
+++ practica3/main.js
@@ -7,3 +7,25 @@ console.log("10-5 =", restar(6, 5));
 console.log("20-8 =", restar(17, 7));
 console.log("7-3 =", restar(9, 4));
 console.log("100-25 =", restar(58, 28));
+
+//Ejercicio BVerrificar usuario
+
+function verificarUsuario(usuario) {
+  return new Promise((resolve, reject) => {
+    if (usuario === "admin") {
+      resolve("Acceso concedido");
+    } else {
+      reject("Acceso denegado");
+    }
+  });
+}
+
+verificarUsuario("admin")
+  .then(res => console.log(res))   //  Acceso concedido
+  .catch(err => console.error(err));
+
+verificarUsuario("Ivan")
+  .then(res => console.log(res))   //  Acceso denegado
+  .catch(err => console.error(err));
+
+
