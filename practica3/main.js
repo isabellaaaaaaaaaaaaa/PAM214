20d4b5c4cffc2e127e0b124716f056fd97868e9b
isabellaaaaaaaaaaaaa <isabella.castal12@gmail.com>
2025-09-25 22:20:41 -0600

    Ejercicio A Practica 3

---
+++ practica3/main.js
@@ -0,0 +1,9 @@
+// Ejercicio A
+
+import { restar } from "./utils.js";
+
+console.log("probando la funcion restar");
+console.log("10-5 =", restar(6, 5));
+console.log("20-8 =", restar(17, 7));
+console.log("7-3 =", restar(9, 4));
+console.log("100-25 =", restar(58, 28));
